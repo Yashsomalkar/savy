783bffb1bafbbf374b5e8376eaf93208970ad35f
LENOVO <yashsomalkar@gmail.com>
2022-01-09 13:46:43 +0530

    shifted to new device

---
+++ app/src/main/java/com/yash/savy/RecyclerViewAdapter.kt
@@ -0,0 +1,11 @@
+package com.yash.savy
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.yash.savy.db.UserEntity
+
+
+
+
