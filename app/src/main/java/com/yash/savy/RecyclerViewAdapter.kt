26f9743b2db5b7bdf7684ffbf3e6cdbf3bd459b3
LENOVO <yashsomalkar@gmail.com>
2022-01-09 19:26:58 +0530

    working app without delete

--- app/src/main/java/com/yash/savy/RecyclerViewAdapter.kt
+++ app/src/main/java/com/yash/savy/RecyclerViewAdapter.kt
@@ -39,7 +39,7 @@ class RecyclerViewAdapter(val listener: RowClickListener): RecyclerView.Adapter<
 
         val tvName = view.tvName
         val tvEmail = view.tvEmail
-        val tvPhone = view.tvPhone
+
         val deleteUserID = view.deleteUserID
 
         fun bind(data: UserEntity) {
@@ -48,7 +48,7 @@ class RecyclerViewAdapter(val listener: RowClickListener): RecyclerView.Adapter<
             tvEmail.text = data.email
 
 
-            tvPhone.text = data.phone
+
 
             deleteUserID.setOnClickListener {
                 listener.onDeleteUserClickListener(data)
